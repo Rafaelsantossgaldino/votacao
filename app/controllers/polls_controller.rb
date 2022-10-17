308037bbe3f00258119c0f998876c4bb3b40e015
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-17 20:32:10 -0300

    modify controller

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,16 @@
 class PollsController < ApplicationController
   def index
-    @poll = Poll.where(finished_at: nil).last
-    @poll_options = @poll.poll_options
+    @poll = Poll.last
+    @poll_options = @poll.poll_options.order(:name)
+  end
+
+  def create
+    @poll = Poll.last
+    @poll_option = @poll.poll_options.find params[:poll_option_id]
+    @poll_option.increment! :votes
+
+    #respond_to do |format|
+    #  format.turbo_stream { render :vote }
+    #end
   end
 end
